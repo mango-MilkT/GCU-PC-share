3631fda42eff2c4269df59068af62924cd245401
mangoMilkT <yuhang_yang@zju.edu.cn>
2024-07-22 20:14:03 +0800

    changed

--- sync_test.py
+++ sync_test.py
@@ -1,5 +1,6 @@
 import torch
 from d2l import torch as d2l
+import torch_gcu
 
 def run(x):
     return [x.mm(x) for _ in range(50)]
@@ -9,18 +10,18 @@ x2 = torch.rand(size=(4000,4000),device='xla:1')
 
 run(x1)
 run(x2)
-torch.cuda.synchronize('xla:0')
-torch.cuda.synchronize('xla:1')
+# torch.cuda.synchronize('xla:0')
+# torch.cuda.synchronize('xla:1')
 
 with d2l.Benchmark('GCU0 time'):
     run(x1)
-    torch.cuda.synchronize('xla:0')
+    # torch.cuda.synchronize('xla:0')
 
 with d2l.Benchmark('GCU1 time'):
     run(x2)
-    torch.cuda.synchronize('xla:1')
+    # torch.cuda.synchronize('xla:1')
 
 with d2l.Benchmark('GCU0 & GCU1 time'):
     run(x1)
     run(x2)
-    torch.cuda.synchronize()
+    # torch.cuda.synchronize()
