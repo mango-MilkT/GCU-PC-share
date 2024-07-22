c68a02b5b8f06a2cb71c973673b148d9f4ba8465
mangoMilkT <yuhang_yang@zju.edu.cn>
2024-07-22 21:14:02 +0800

    change stream test

--- stream_test.py
+++ stream_test.py
@@ -3,5 +3,7 @@ import torch_gcu
 
 device = 'xla:0'
 
+stream = torch_gcu.get_exe_default_stream(device=device)
+
 print('******************Test result******************')
-print(torch_gcu.get_exe_default_stream(device=device))
+print(stream)
