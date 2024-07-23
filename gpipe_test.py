9b91c3d28c6b0dc56572966ff21d5f9acf7c22d7
mangoMilkT <yuhang_yang@zju.edu.cn>
2024-07-23 14:54:22 +0800

    change gpipe test

--- gpipe_test.py
+++ gpipe_test.py
@@ -21,29 +21,38 @@ loss = nn.CrossEntropyLoss(reduction='none')
 trainer = torch.optim.SGD(model.parameters(), lr=lr)
 
 train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
+input("data loaded!")
 
 in_device = model.devices[0]
 out_device = model.devices[-1]
+input("in & out device set!")
 
 animator = d2l.Animator(xlabel='epoch', xlim=[1,num_epochs], ylim=[0.3,0.9], legend=['train loss', 'train acc', 'test acc'])
 for epoch in range(num_epochs):
     if isinstance(model, torch.nn.Module):
         model.train()
+        input("train mode on!")
     metric = d2l.Accumulator(3)
     for X, y in train_iter:
         X = X.to(in_device, non_blocking=True)
         y = y.to(out_device, non_blocking=True)
+        input("to device complete!")
         y_hat = model(X)
+        input("forward compute complete!")
         l = loss(y_hat, y)
         if isinstance(trainer, torch.optim.Optimizer):
             trainer.zero_grad()
+            input("zero grad complete!")
             l.mean().backward()
+            input("backward compute complete!")
             trainer.step()
+            input("optim step complete!")
         metric.add(float(l.sum()), d2l.accuracy(y_hat, y), y.numel())
     train_metrics = metric[0] / metric[2], metric[1] / metric[2]
     
     if isinstance(model, nn.Module):
         model.eval()  # Set the model to evaluation mode
+        input("eval mode on!")
     metric = d2l.Accumulator(2)
     with torch.no_grad():
         for X, y in test_iter:
@@ -60,3 +69,5 @@ train_loss, train_acc = train_metrics
 assert train_loss < 0.5, train_loss
 assert train_acc <= 1 and train_acc > 0.7, train_acc
 assert test_acc <= 1 and test_acc > 0.7, test_acc
+
+input("all complete!")
